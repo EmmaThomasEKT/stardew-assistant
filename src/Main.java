8016b33cba5ba0768f637346e6a23e759dbf1d79
EmmaThomasEKT <ethomas210200@gmail.com>
2025-08-01 16:56:06 +1000

    Fix commit tracking with correct user email

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // Basic seasonal budgeting for major purchases (seeds, farm animals and buildings)
 // Calculate your profit based on animals (high/low heart level), seeds,
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,7 +15,7 @@ public class Main {
         boolean running = true;
 
         while (running) {
-            System.out.println("Welcome to Stardew Assistant!\nHere are the available tools:");
+            System.out.println("Welcome to Stardew Assistant!\nPlease select a tool (1/2/3/4):");
             System.out.println("""
                             1. Seasonal Budget Planner
                             2. Profit Calculator
@@ -26,10 +30,10 @@ public class Main {
                     seasonalBudget(scanner);
                     break;
                 case 2:
-                    profitCalculator(scanner);
+                    profitCalculator(scanner); // not started
                     break;
                 case 3:
-                    friendshipTracker(scanner);
+                    friendshipTracker(scanner); // not started
                     break;
                 case 4:
                     System.out.println("Thanks for using Stardew Assistant!");
@@ -72,12 +76,14 @@ public class Main {
                         pierreShop(scanner);
                         break;
                     case 4:
-                        shoppingList();
+                        shoppingList(scanner);
+                        break;
                     case 5:
                         calculateTotal();
                         break;
                     case 6:
                         budgeting = false;
+                        // causes error. try to find way back to main without calling main() ?
                         break;
                     default:
                         System.out.println("Invalid choice.");
@@ -121,27 +127,65 @@ public class Main {
         }
 
         public static void houseUpgrades(Scanner scanner) {
-
+            System.out.println("Under Construction!");
         }
 
         public static void houseRenovations(Scanner scanner) {
-
+            System.out.println("Under Construction!");
         }
 
         public static void marnieRanch(Scanner scanner) {
-
+            System.out.println("Under Construction!");
         }
 
         public static void pierreShop(Scanner scanner) {
-
+            System.out.println("Under Construction!");
         }
 
-        public static void shoppingList() {
+        public static void shoppingList(Scanner scanner) {
+            // add option to clear list
+            try (BufferedReader br = new BufferedReader(new FileReader("ShoppingList.txt"))) {
+                String line = br.readLine();
 
+                if (line != null) {
+                    System.out.println("--- Shhopping List ---\n");
+                    do {
+                        System.out.println(line);
+                        line = br.readLine();
+                    } while (line != null);
+                } else {
+                    System.out.println("Your shopping list is empty!");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Please select an option from below (1/2/3):\n" +
+                                """
+                                    1. Clear List
+                                    2. Calculate Total
+                                    3. Return to Seasonal Budget Menu
+                                    """);
+
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    //clear the list
+                    break;
+                case 2:
+                    calculateTotal();
+                    break;
+                case 3:
+                    seasonalBudget(scanner);
+                    break;
+            }
         }
 
         public static void calculateTotal() {
-
+            // create a hashmap with the string item and integer quanity
+            // find a way to combine string items
         }
 
         public static void profitCalculator(Scanner scanner) {
